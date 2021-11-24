9d54f4b4fb0a38b864f9b943038dbae5b621a88d
tiyas aria <tiyasaria123@gmail.com>
2021-11-24 20:44:44 +0700

    Initial commit

---
+++ app/src/main/java/com/idn/arika/doaseharihariapp/model/ModelDoa.kt
@@ -0,0 +1,10 @@
+package com.idn.arika.doaseharihariapp.model
+
+class ModelDoa {
+    var strId: String? = null
+    var strTitle: String? = null
+    var strArabic: String? = null
+    var strLatin: String? = null
+    var strTranslation: String? = null
+
+}
